d11f2b1b283e0e3472740d1eb4dc854b504b2894
Hector Sempere <hsempere@teralco.com>
2024-05-31 09:49:50 +0200

    add getById, create, update, delete and search

--- src/main/java/com/project/spaceship/controller/SpaceshipController.java
+++ src/main/java/com/project/spaceship/controller/SpaceshipController.java
@@ -3,8 +3,14 @@ package com.project.spaceship.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.spaceship.model.Spaceship;
@@ -20,6 +26,36 @@ public class SpaceshipController {
     @GetMapping
     public List<Spaceship> getAll() {
         return this.spaceshipRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Spaceship getById(@PathVariable Long id) {
+        return this.spaceshipRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("/search")
+    public List<Spaceship> searchByName(@RequestParam String name) {
+        return this.spaceshipRepository.findByNameContainingIgnoreCase(name);
+    }
+    
+    @PostMapping
+    public Spaceship create(@RequestBody Spaceship spaceship) {
+        return this.spaceshipRepository.save(spaceship);
+    }
+
+    @PutMapping("/{id}")
+    public Spaceship update(@PathVariable Long id, @RequestBody Spaceship spaceshipDetails) {
+        Spaceship spaceship = this.spaceshipRepository.findById(id).orElse(null);
+        if (spaceship != null) {
+            spaceship.setName(spaceshipDetails.getName());
+            return this.spaceshipRepository.save(spaceship);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+    	this.spaceshipRepository.deleteById(id);
     }
 
 }
--- src/main/java/com/project/spaceship/repository/SpaceshipRepository.java
+++ src/main/java/com/project/spaceship/repository/SpaceshipRepository.java
@@ -1,7 +1,11 @@
 package com.project.spaceship.repository;
 
+import java.util.List;
+
 import com.project.spaceship.model.Spaceship;
 
 public interface SpaceshipRepository extends BaseRepository<Spaceship> {
 
+	List<Spaceship> findByNameContainingIgnoreCase(String name);
+	
 }
